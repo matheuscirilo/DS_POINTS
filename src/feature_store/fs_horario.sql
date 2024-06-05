7851fe6ae8ba7852f8c892cedc93d2d111ef1775
matheuscirilo <matheuscirilodasilva@gmail.com>
2024-06-05 10:47:58 -0300

    fix: fs_horario

--- src/feature_store/fs_horario.sql
+++ src/feature_store/fs_horario.sql
@@ -9,97 +9,94 @@ with tb_transactions_hour AS (
         AND dtTransaction >= DATE('{date}', '-21 day')
 ),
 tb_share AS (
-SELECT idCustomer,
-    SUM(
-        CASE
-            WHEN hour >= 8
-            AND hour < 12 THEN abs(pointsTransaction)
-            ELSE 0
-        END
-    ) AS qtdPointsManha,
-    SUM(
-        CASE
-            WHEN hour >= 12
-            AND hour < 18 THEN abs(pointsTransaction)
-            ELSE 0
-        END
-    ) AS qtdPointsTarde,
-    SUM(
-        CASE
-            WHEN hour >= 18
-            AND hour <= 23 THEN abs(pointsTransaction)
-            ELSE 0
-        END
-    ) AS qtdPointsNoite,
-    1.0 * SUM(
-        CASE
-            WHEN hour >= 8
-            AND hour < 12 THEN abs(pointsTransaction)
-            ELSE 0
-        END
-    ) / SUM(ABS(pointsTransaction)) AS pctPointsManha,
-    1.0 * SUM(
-        CASE
-            WHEN hour >= 12
-            AND hour < 18 THEN abs(pointsTransaction)
-            ELSE 0
-        END
-    ) / SUM(ABS(pointsTransaction)) AS pctPointsTarde,
-    1.0 * SUM(
-        CASE
-            WHEN hour >= 18
-            AND hour <= 23 THEN abs(pointsTransaction)
-            ELSE 0
-        END
-    ) / SUM(ABS(pointsTransaction)) AS pctPointsNoite,
-    SUM(
-        CASE
-            WHEN hour >= 8
-            AND hour < 12 THEN 1
-            ELSE 0
-        END
-    ) AS qtdTransactionsManha,
-    SUM(
-        CASE
-            WHEN hour >= 12
-            AND hour < 18 THEN 1
-            ELSE 0
-        END
-    ) AS qtdTransactionsTarde,
-    SUM(
-        CASE
-            WHEN hour >= 18
-            AND hour <= 23 THEN 1
-            ELSE 0
-        END
-    ) AS qtdTransactionsNoite,
-    1.0 * SUM(
-        CASE
-            WHEN hour >= 8
-            AND hour < 12 THEN 1
-            ELSE 0
-        END
-    ) / SUM(ABS(pointsTransaction)) AS pctTransactionsManha,
-    1.0 * SUM(
-        CASE
-            WHEN hour >= 12
-            AND hour < 18 THEN 1
-            ELSE 0
-        END
-    ) / SUM(ABS(pointsTransaction)) AS pctTransactionsTarde,
-    1.0 * SUM(
-        CASE
-            WHEN hour >= 18
-            AND hour <= 23 THEN 1
-            ELSE 0
-        END
-    ) / SUM(ABS(pointsTransaction)) AS pctTransactionsNoite
-FROM tb_transactions_hour
-GROUP BY idCustomer
-
+    SELECT idCustomer,
+        SUM(
+            CASE
+                WHEN hour >= 8
+                AND hour < 12 THEN abs(pointsTransaction)
+                ELSE 0
+            END
+        ) AS qtdPointsManha,
+        SUM(
+            CASE
+                WHEN hour >= 12
+                AND hour < 18 THEN abs(pointsTransaction)
+                ELSE 0
+            END
+        ) AS qtdPointsTarde,
+        SUM(
+            CASE
+                WHEN hour >= 18
+                AND hour <= 23 THEN abs(pointsTransaction)
+                ELSE 0
+            END
+        ) AS qtdPointsNoite,
+        1.0 * SUM(
+            CASE
+                WHEN hour >= 8
+                AND hour < 12 THEN abs(pointsTransaction)
+                ELSE 0
+            END
+        ) / SUM(ABS(pointsTransaction)) AS pctPointsManha,
+        1.0 * SUM(
+            CASE
+                WHEN hour >= 12
+                AND hour < 18 THEN abs(pointsTransaction)
+                ELSE 0
+            END
+        ) / SUM(ABS(pointsTransaction)) AS pctPointsTarde,
+        1.0 * SUM(
+            CASE
+                WHEN hour >= 18
+                AND hour <= 23 THEN abs(pointsTransaction)
+                ELSE 0
+            END
+        ) / SUM(ABS(pointsTransaction)) AS pctPointsNoite,
+        SUM(
+            CASE
+                WHEN hour >= 8
+                AND hour < 12 THEN 1
+                ELSE 0
+            END
+        ) AS qtdTransactionsManha,
+        SUM(
+            CASE
+                WHEN hour >= 12
+                AND hour < 18 THEN 1
+                ELSE 0
+            END
+        ) AS qtdTransactionsTarde,
+        SUM(
+            CASE
+                WHEN hour >= 18
+                AND hour <= 23 THEN 1
+                ELSE 0
+            END
+        ) AS qtdTransactionsNoite,
+        1.0 * SUM(
+            CASE
+                WHEN hour >= 8
+                AND hour < 12 THEN 1
+                ELSE 0
+            END
+        ) / SUM(ABS(pointsTransaction)) AS pctTransactionsManha,
+        1.0 * SUM(
+            CASE
+                WHEN hour >= 12
+                AND hour < 18 THEN 1
+                ELSE 0
+            END
+        ) / SUM(ABS(pointsTransaction)) AS pctTransactionsTarde,
+        1.0 * SUM(
+            CASE
+                WHEN hour >= 18
+                AND hour <= 23 THEN 1
+                ELSE 0
+            END
+        ) / SUM(ABS(pointsTransaction)) AS pctTransactionsNoite
+    FROM tb_transactions_hour
+    GROUP BY idCustomer
 )
-
-SELECT '{date}' AS dtRef, *
-
-
+SELECT '{date}' AS dtRef,
+    *
 FROM tb_share
